e458c31158c2b09d2fa700b4ba3006cc0f2b8148
Navya Aneja <185716508+navyaanejathegreat@users.noreply.github.com>
2025-01-10 08:15:37 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,21 @@
+print(12345)
+print("hello world")
+a=10
+print(type(a))
+a=10.5
+print(type(a))
+x="syed sir"
+print(type(x))
+x="navya"
+print(type(x))
+x=10
+print(id(x))
+x=x+1
+print(id(x))
+y="hello"
+print(id(y)
+y=y+" world"
+print(id(y))
+l1=[1,2,3]
+print(id(l1))
+l1added(4)
